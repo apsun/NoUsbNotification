f67cfcbf0f8758b3d54f7b2581ff07cd959421a8
Andrew Sun <0xandrewsun@gmail.com>
2016-02-14 11:20:50 -0600

    Improve device compatibility

--- src/com/crossbowffs/nousbnotification/Hook.java
+++ src/com/crossbowffs/nousbnotification/Hook.java
@@ -13,25 +13,41 @@ public class Hook implements IXposedHookLoadPackage {
             return;
         }
 
-        XposedHelpers.findAndHookMethod(
-            "com.android.server.usb.UsbDeviceManager$UsbHandler", lpparam.classLoader,
-            "updateAdbNotification", new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                    Log.v(TAG, "In UsbHandler#updateAdbNotification()");
-                    param.setResult(null);
-                }
-            });
+        String[] classNames = {
+            "com.android.server.usb.UsbDeviceManager$UsbHandler",
+            "com.android.server.usb.UsbDeviceManagerEx$UsbHandlerEx"
+        };
 
-        XposedHelpers.findAndHookMethod(
-            "com.android.server.usb.UsbDeviceManager$UsbHandler", lpparam.classLoader,
-            "updateUsbNotification", new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                    Log.v(TAG, "In UsbHandler#updateUsbNotification()");
-                    param.setResult(null);
-                }
-            });
+        for (String className : classNames) {
+            Class<?> cls;
+            try {
+                cls = XposedHelpers.findClass(className, lpparam.classLoader);
+            } catch (XposedHelpers.ClassNotFoundError e) {
+                continue;
+            }
+
+            Log.i(TAG, "Hooking USB handler class: " + className);
+
+            try {
+                XposedHelpers.findAndHookMethod(cls, "updateAdbNotification", new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        Log.v(TAG, "In UsbHandler#updateAdbNotification()");
+                        param.setResult(null);
+                    }
+                });
+
+                XposedHelpers.findAndHookMethod(cls, "updateUsbNotification", new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        Log.v(TAG, "In UsbHandler#updateUsbNotification()");
+                        param.setResult(null);
+                    }
+                });
+            } catch (Throwable e) {
+                Log.e(TAG, "Failed to hook USB handler methods", e);
+            }
+        }
 
         Log.i(TAG, "NoUsbNotification init successful!");
     }
